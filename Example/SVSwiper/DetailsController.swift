fcf570bd3e799281c6625688cd18d6ccf1461eda
WeCarryBags <vinayak.sareen@wecarrybags.co.uk>
2022-07-17 17:41:01 -0400

    state captured

---
+++ Example/SVSwiper/DetailsController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailsController.swift
+//  SVSwiper_Example
+//
+//  Created by DataBunker on 2022-07-17.
+//  Copyright © 2022 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class DetailsController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .yellow
+    }
+}
--- Example/SVSwiper/ViewController.swift
+++ Example/SVSwiper/ViewController.swift
@@ -2,16 +2,33 @@
 import UIKit
 import SVSwiper
 
-class ViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
+class ViewController: UIViewController, SVSwiperDelegate {
+    
+    
+    
+    
+    fileprivate func setupView() {
         let controller = SVSwiperController(backgroundColor: .white, details: [
             .init(title: "Maximise Profits", description: "Gain with our internal tools, we are here to support your company at every stage", image: #imageLiteral(resourceName: "2")),
             .init(title: "Invest in Stocks", description: "We provide great insights, on which stock to buy and sell at any moment of time", image: #imageLiteral(resourceName: "3")),
             .init(title: "Develop Connections" , description: "Conntect with community of experts in the field of stock market", image: #imageLiteral(resourceName: "2")),
             .init(title: "Develop Connections" , description: "Conntect with community of experts in the field of stock market", image: #imageLiteral(resourceName: "3"))
         ])
-        controller.configureSlider(parentViewController: self)
+        
+        controller.delegate = self
+        
+        
+       
+        let navigationController = UINavigationController(rootViewController: self)
+        navigationController.navigationBar.prefersLargeTitles = true
+        controller.configureSlider(parentViewController: self, secondaryController: navigationController)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        self.navigationItem.title = "Montreal"
+        setupView()
     }
 }
 
